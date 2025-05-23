70185890fc1519189ab267c533f63572a2343502
Aditya @ArchLinux <132184385+adityadeshlahre@users.noreply.github.com>
2025-05-23 10:21:31 +0530

    noice + inc-rename plugin added

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,42 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+-- return {}
+
+return {
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    opts = {
+      -- add any options here
+      presets = {
+        command_palette = true,
+      },
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      'MunifTanjim/nui.nvim',
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      'rcarriga/nvim-notify',
+    },
+  },
+  {
+    'smjonas/inc-rename.nvim',
+    cmd = 'IncRename',
+    keys = {
+      {
+        '<leader>rn',
+        function()
+          return ':IncRename ' .. vim.fn.expand '<cword>'
+        end,
+        desc = 'Incremental rename',
+        mode = 'n',
+        noremap = true,
+        expr = true,
+      },
+    },
+    config = true,
+  },
+}
